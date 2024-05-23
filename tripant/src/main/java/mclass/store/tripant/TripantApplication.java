fbb89a6289838fc407594b3a90426c824541ca49
seojw0730 <seojw0730@naver.com>
2024-05-23 16:13:51 +0900

    tripant 초기 설정

---
+++ tripant/src/main/java/mclass/store/tripant/TripantApplication.java
@@ -0,0 +1,13 @@
+package mclass.store.tripant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripantApplication.class, args);
+	}
+
+}
---
+++ tripant/src/test/java/mclass/store/tripant/TripantApplicationTests.java
@@ -0,0 +1,13 @@
+package mclass.store.tripant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
