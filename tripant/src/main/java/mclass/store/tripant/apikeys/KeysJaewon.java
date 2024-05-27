72538f41c0091e8d6fff23a661da6742226f149d
seojw0730 <seojw0730@naver.com>
2024-05-27 12:22:00 +0900

    재원)~.gitignore 복구

---
+++ tripant/src/main/java/mclass/store/tripant/apikeys/KeysJaewon.java
@@ -0,0 +1,46 @@
+package mclass.store.tripant.apikeys;
+
+import java.net.URLEncoder;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+
+@Getter
+@Component
+@PropertySource("classpath:/keyproperties/apikeys.properties")
+public class KeysJaewon {
+	
+	private final String isLocal = "local";// local, tripant
+	
+	//도메인
+	@Value("${domain."+isLocal+"}")
+	private String domain;
+	
+	//카카오 로그인/로그아웃
+	@Value("${kakao.login."+isLocal+".url}")
+	private String kakaoLoginUrl;
+	@Value("${kakao.login.clientid}")
+	private String kakaoClientId;
+	@Value("${kakao.login.clientsecret}")
+	private String kakaoClientSecret;
+	@Value("${kakao.login."+isLocal+".redirecturi}")
+	private String kakaoLoginRedirectUri;
+	@Value("${kakao.logout."+isLocal+".redirecturi}")
+	private String kakaoLogoutRedirectUri;
+	
+	//네이버 로그인/로그아웃
+	@Value("${naver.login."+isLocal+".url}")
+	private String naverLoginUrl;
+	@Value("${naver.login.clientid}")
+	private String naverLoginClientId;
+	@Value("${naver.login.clientsecret}")
+	private String naverLoginClientSecret;
+	@Value("${naver.login."+isLocal+".callbackurl}")
+	private String naverCallbackUrl;
+	
+	
+}
