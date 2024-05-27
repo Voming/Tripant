05b9cfcee47ebe282415bbd725f7245007a1f2e3
Voming <ebomin1234@gmail.com>
2024-05-27 15:56:20 +0900

    sample layout 헤더 푸터

--- tripant/src/main/java/mclass/store/tripant/trip/controller/MapController.java
+++ tripant/src/main/java/mclass/store/tripant/trip/controller/MapController.java
@@ -12,4 +12,10 @@ public class MapController {
 		//model.addAttribute();
 		return "trip/trip";
 	}
+	
+	@GetMapping("/sample")
+	public String sample(Model model) {
+		//model.addAttribute();
+		return "sample_layout";
+	}
 }
