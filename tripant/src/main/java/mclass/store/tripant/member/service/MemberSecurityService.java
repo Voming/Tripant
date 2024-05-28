1d3b0ce9dbbdb00ffd10a1a3c2f9b43c1d65b59f
seojw0730 <seojw0730@naver.com>
2024-05-28 17:38:44 +0900

    재원)로그인 기능 진행중

--- tripant/src/main/java/mclass/store/tripant/member/service/MemberSecurityService.java
+++ tripant/src/main/java/mclass/store/tripant/member/service/MemberSecurityService.java
@@ -1,5 +1,22 @@
 package mclass.store.tripant.member.service;
 
-public class MemberSecurityService {
+import java.util.Optional;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import mclass.store.tripant.member.model.repository.MemberRepository;
+
+//@RequiredArgsConstructor
+//@Service
+public class MemberSecurityService implements UserDetailsService {
+	private final MemberRepository memberRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String memEmail) throws UsernameNotFoundException {
+		return null;
+	}
 }
