315f03247a381d34ddee861e46dbc6db6c956bdd
JJOGGAN <withmelona1@gmail.com>
2024-05-28 12:55:10 +0900

    api에서 값 받아와서 디스플레이하기
    길찾기 api를 이용하여 이동시간 값 가져오기
    controller까지는 값 받아왔으나 html에서 값을 받아오지 못하는 상황 수정할 예정

--- tripant/src/main/java/mclass/store/tripant/trip/controller/TripController.java
+++ tripant/src/main/java/mclass/store/tripant/trip/controller/TripController.java
@@ -1,5 +1,12 @@
 package mclass.store.tripant.trip.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,19 +24,54 @@ public class TripController {
 	
 	
 	//sts꺼
-//	@Value("${kakao.map.rest.api}")
-//	@GetMapping("/map/getduration")
-//	public String getduration(
-//			@RequestParam double startLat,
-//			@RequestParam double startLng,
-//			@RequestParam double endLat,
-//			@RequestParam double endLng,
-//			Model model) {
-//		
-//		String urlStr="https://apis-navi.kakaomobility.com/v1/directions?"
-//				+ "origin="+12+","+33
-//				+ "destination=127.10823557165544,37.401928707331656&priority=TIME";
-//		
-//		return "trip/trip";
-//	}
+	@Value("${kakao.map.rest.api}")
+	private String apikey;
+	
+	@GetMapping("/map/getduration")
+	public String getduration(
+			@RequestParam double startLng,
+			@RequestParam double startLat,
+			@RequestParam double endLng,
+			@RequestParam double endLat,
+			Model model) {
+		System.out.println(">>>>>>>>>>controller 들어옴");
+		System.out.println(endLat);
+		String aurlStr= String.format("https://apis-navi.kakaomobility.com/v1/directions?origin=%f,%f&destination=%f,%f&priority=TIME", startLng,startLat,endLng,endLat);
+		System.out.println(aurlStr);
+		System.out.println(apikey);
+		
+		try {
+			URL url = new URL(aurlStr);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Authorization", apikey);
+			conn.setRequestProperty("Content-Type", "application/json");
+			
+			System.out.println(">>>>>>>>>>>>>>>>>>> conn \n\n");
+			
+			int responseCode = conn.getResponseCode();
+			System.out.println("******************             아래 확인");
+			System.out.println(responseCode);
+			if (responseCode == HttpURLConnection.HTTP_OK) {
+				BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				String inputLine;
+				StringBuilder response = new StringBuilder();
+				while((inputLine = br.readLine()) != null) {
+					response.append(inputLine);
+					
+				}
+				System.out.println("\n response \n "+response);
+				br.close();
+				
+				model.addAttribute("directions", response.toString() );
+			}else {
+				model.addAttribute("directions", " \n >>>> 에러났어요 : " + responseCode);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("\n\n >>>>>>>>>>>>>>>>ERROR 확인해주세요<<<<<<<<<<<<<<<<");
+		}
+		return "trip/trip";
+	}
 }
