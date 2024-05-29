61bca46be0bde50017d5b884e50d995654c8fc8a
seojw0730 <seojw0730@naver.com>
2024-05-29 12:19:25 +0900

    재원)~js폴더 추가

---
+++ tripant/src/main/resources/static/js/login/button-hover.js
@@ -0,0 +1,13 @@
+<script>
+$(loadedHandler);
+function loadedHandler(){
+	//버튼 호버 css
+	$('.btn').hover(function(){
+		$(this).css('background-color', 'black');
+		$(this).css('color', 'white');
+	}, function(){
+		$(this).css('background-color', 'lightgrey');
+		$(this).css('color', 'black');
+	});
+}
+</script>
