600c35d95ff858e2eb23624670a8e18fc46949d4
seojw0730 <seojw0730@naver.com>
2024-05-29 15:52:53 +0900

    재원)로그인 실패 시 오류 메시지 출력 완료

--- tripant/src/main/java/mclass/store/tripant/login/controller/CustomAuthFailureHandler.java
+++ tripant/src/main/java/mclass/store/tripant/login/controller/CustomAuthFailureHandler.java
@@ -2,7 +2,6 @@ package mclass.store.tripant.login.controller;
 
 import java.io.IOException;
 import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
@@ -24,21 +23,17 @@ public class CustomAuthFailureHandler extends SimpleUrlAuthenticationFailureHand
 		String error;
 		if(exception instanceof BadCredentialsException) {
 			error = "BadCredentialsException";
-//			errorMsg = "이메일 또는 비밀번호가 맞지 않습니다. 다시 확인해주세요.";
 		}else if(exception instanceof InternalAuthenticationServiceException) {
-			error = "시스템 문제로 인해 요청을 처리할 수 없습니다. 관리자에게 문의해주세요.";
-//			error = "시스템 문제로 인해 요청을 처리할 수 없습니다. 관리자에게 문의해주세요.";
+			error = "InternalAuthenticationServiceException";
 		}else if(exception instanceof UsernameNotFoundException) {
-			error = "계정이 존재하지 않습니다. 회원가입 진행 후 로그인 해주세요.";
+			error = "UsernameNotFoundException";
 		}else if(exception instanceof AuthenticationException) {
-			error = "탈퇴 처리된 회원입니다. 관리자에게 문의해주세요.";
+			error = "AuthenticationException";
 		}else {
-			error = "알 수 없는 이유로 로그인에 실패하였습니다. 관리자에게 문의해주세요.";
+			error = "others";
 		}
-//		byte[] bytes = errorMsg.getBytes(StandardCharsets.UTF_8);
 		error = URLEncoder.encode(error, "UTF-8");
 		setDefaultFailureUrl("/login?error=true&exception="+error);
-		request.setAttribute("aaa", "bbb");
 	
 		super.onAuthenticationFailure(request, response, exception);
 	}
--- tripant/src/main/java/mclass/store/tripant/login/controller/LoginController.java
+++ tripant/src/main/java/mclass/store/tripant/login/controller/LoginController.java
@@ -30,18 +30,30 @@ public class LoginController {
 	@GetMapping("/login")
 	public String login(@RequestParam(value = "error", required = false)String error, 
 						@RequestParam(value = "exception", required = false)String exception, 
-//						AuthenticationException exception2,
-//			RedirectAttributes rttr,
-						HttpServletRequest request,
 						Model model) {
-		System.out.println("==========="+ request.getAttribute("aaa"));
-//		No primary or single unique constructor found
-//for class org.springframework.security.core.AuthenticationException
+		String msg="";
+		if(exception != null) {
+			switch(exception) {
+			case "BadCredentialsException":
+				msg = "이메일 또는 비밀번호가 맞지 않습니다. 다시 확인해주세요.";
+				break;
+			case "InternalAuthenticationServiceException":
+				msg = "시스템 문제로 인해 요청을 처리할 수 없습니다. 관리자에게 문의해주세요.";
+				break;
+			case "UsernameNotFoundException":
+				msg = "계정이 존재하지 않습니다. 회원가입 진행 후 로그인 해주세요.";
+				break;
+			case "AuthenticationException":
+				msg = "탈퇴 처리된 회원입니다. 관리자에게 문의해주세요.";
+				break;
+			default:
+				msg = "알 수 없는 이유로 로그인에 실패하였습니다. 관리자에게 문의해주세요.";
+				break;
+			}
+		}
 		
-//		System.out.println(exception2);
 		model.addAttribute("error", error);
-//		String exception = new String(exceptionArr, StandardCharsets.UTF_8);
-		model.addAttribute("exception", exception);
+		model.addAttribute("exception", msg);
 		return "login/login";
 	}
 	
