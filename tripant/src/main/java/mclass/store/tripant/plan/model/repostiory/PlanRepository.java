d6cc0e1b9041e986ed99addf04aa441bf8fb490a
Voming <ebomin1234@gmail.com>
2024-05-30 14:04:53 +0900

    장소 entity생성

--- tripant/src/main/java/mclass/store/tripant/plan/model/repostiory/PlanRepository.java
+++ tripant/src/main/java/mclass/store/tripant/plan/model/repostiory/PlanRepository.java
@@ -9,8 +9,8 @@ import mclass.store.tripant.plan.domain.AreaNameEntity;
 
 @Mapper
 public interface PlanRepository {
-	public int selectPlanCount();
-	public int selectMemCount();
-	public List<AreaNameEntity> selectAreaNameList();
-	public List<AreaNameEntity> selectAreaFindList(String findArea);
+	public int selectPlanCount(); //생성된 일정 수
+	public int selectMemCount();  //가입한 멤버 수
+	public List<AreaNameEntity> selectAreaNameList(); //지역명 리스트
+	public List<AreaNameEntity> selectAreaFindList(String findArea);  //지역명 검색
 }
---
+++ tripant/src/main/java/mclass/store/tripant/schedule/domain/PlaceApiEntity.java
@@ -0,0 +1,34 @@
+package mclass.store.tripant.schedule.domain;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+import mclass.store.tripant.plan.domain.PlanEntity;
+
+@Data
+@Component
+public class PlaceApiEntity {
+	private String add1;
+	private String add2;
+	private Integer areacode;
+	private String booktour;
+	private String cat1;
+	private String cat2;
+	private String cat3;
+	private Integer contentid;
+	private String contenttypeid;
+	private String createtime;
+	
+	private String firstimage;
+	private String firstimage2;
+	private String cpyrhtDivCd;
+	private String mapx;
+	private String mapy;
+	private String mlevel;
+	private String modifiedtime;
+	private String sigungucode;
+	private String tel;
+	private String title;
+
+	private Integer zipcode;
+}
