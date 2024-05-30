c28b11923a6ab5fd84b067411912b148bb03960f
sobae17 <sobae17@naver.com>
2024-05-30 16:37:04 +0900

    여행기 게시판 더미 글 연동 확인

--- tripant/src/main/java/mclass/store/tripant/diary/model/repository/DiaryRepository.java
+++ tripant/src/main/java/mclass/store/tripant/diary/model/repository/DiaryRepository.java
@@ -1,8 +1,12 @@
 package mclass.store.tripant.diary.model.repository;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import mclass.store.tripant.diary.domain.DiaryBoardEntity;
 
 @Mapper
 public interface DiaryRepository {
-	public String selectDiaryList();
+	public List<DiaryBoardEntity> selectDiaryList();
 }
--- tripant/src/main/java/mclass/store/tripant/diary/service/DiaryService.java
+++ tripant/src/main/java/mclass/store/tripant/diary/service/DiaryService.java
@@ -1,14 +1,21 @@
 package mclass.store.tripant.diary.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import mclass.store.tripant.diary.domain.DiaryBoardEntity;
 import mclass.store.tripant.diary.model.repository.DiaryRepository;
 
 @Service
 public class DiaryService {
-//	@Autowired
-//	private DiaryRepository diaryRepository;
+	@Autowired
+	private DiaryRepository diaryRepository;
+	
+	public List<DiaryBoardEntity> selectDiaryList(){
+		return diaryRepository.selectDiaryList();
+	}
 	
 	
 }
