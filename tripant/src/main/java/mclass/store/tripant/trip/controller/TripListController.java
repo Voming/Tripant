64c1d867a8870855ae2b8e2c78fe973e59e87308
JJOGGAN <withmelona1@gmail.com>
2024-05-30 12:41:43 +0900

    삭제 모달
    controller url 생성
    css추가
    sweetalert2으로 모달 생성

--- tripant/src/main/java/mclass/store/tripant/trip/controller/TripListController.java
+++ tripant/src/main/java/mclass/store/tripant/trip/controller/TripListController.java
@@ -3,7 +3,9 @@ package mclass.store.tripant.trip.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = "/plan/list")
@@ -11,6 +13,12 @@ public class TripListController {
 	
 	@GetMapping("")
 	public String mainList(Model model) {
+		
+		return "trip/tripList";
+	}
+	@PostMapping("/delete")//ajax
+	@ResponseBody
+	public String planDelete(Model model) {
 		
 		return "trip/tripList";
 	}
