5664264dd2cdfa37e5fdc0873ad481d278ee865c
sobae17 <sobae17@naver.com>
2024-05-30 12:18:53 +0900

    여행기 mybite 연결

---
+++ tripant/src/main/java/mclass/store/tripant/diary/service/DiaryService.java
@@ -0,0 +1,14 @@
+package mclass.store.tripant.diary.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import mclass.store.tripant.diary.model.repository.DiaryRepository;
+
+@Service
+public class DiaryService {
+	@Autowired
+	private DiaryRepository diaryRepository;
+	
+	
+}
