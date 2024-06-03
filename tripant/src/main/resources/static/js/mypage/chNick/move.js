b24001654db99d13bf15d71d116b8de9553b2c4a
seojw0730 <seojw0730@naver.com>
2024-06-03 15:02:59 +0900

    재원)~닉네임 변경
    1. js 파일 분리

---
+++ tripant/src/main/resources/static/js/mypage/chNick/move.js
@@ -0,0 +1,21 @@
+//마이페이지 돌아가기
+function backClickHandler(){
+	location.href = '/my/home';
+}
+//닉네임 저장
+function saveClickHandler(){
+	$.ajax({
+		url: '/save/nick', 
+		type: 'post', 
+		data: $('#memNick'), 
+		success: function(result){
+			if(result == 1){
+				alert('닉네임이 변경되었습니다.');
+				location.href = '/my/home';
+			}else{
+				alert('닉네임 변경 중 오류가 발생했습니다.\n관리자에게 문의해주시기 바랍니다.');
+			}
+		}, 
+		error: ajaxErrorHandler
+	});
+}
