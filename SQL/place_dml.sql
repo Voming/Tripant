15ed05f21b7dda23f6b108cdf592593a2153fdf0
Voming <68582465+Voming@users.noreply.github.com>
2024-06-04 01:32:25 +0900

    일정 날짜선택 레이아웃 구현중

--- SQL/place_dml.sql
+++ SQL/place_dml.sql
@@ -1,6 +1,6 @@
 commit;
 
-select * from place;
+select * from area;
 
 desc place_move_time;
 
@@ -9,3 +9,7 @@ TRUNCATE TABLE place_move_time;
 select type, contentid, mapx, mapy,areacode from place where areacode=1 order by contentid;
 
 select count(*) from place where areacode=1;
+
+select * from area where area_name = '서울특별시';
+
+select * from member;
