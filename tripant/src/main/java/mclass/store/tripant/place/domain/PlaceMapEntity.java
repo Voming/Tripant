5a9e2b255293ff5b7a2fca5d97e6f694610543ed
Voming <ebomin1234@gmail.com>
2024-06-04 17:51:14 +0900

    출발지, 목적지 리스트 만들기 + kakao api  이동시간 받아오기

--- tripant/src/main/java/mclass/store/tripant/place/domain/PlaceMapEntity.java
+++ tripant/src/main/java/mclass/store/tripant/place/domain/PlaceMapEntity.java
@@ -4,16 +4,17 @@ import org.springframework.stereotype.Component;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Data
 @Component
 @NoArgsConstructor
 @AllArgsConstructor
-public class PlaceAreaEntity { //지역 2개씩 묶을때 사용
+@Getter
+public class PlaceMapEntity {
 	private Integer type;
 	private Integer contentid;
-	private String mapx;
-	private String mapy;
-	private Integer areacode;
+	private String mapx; //경도
+	private String mapy; //위도
 }
--- tripant/src/main/java/mclass/store/tripant/plan/model/repostiory/TimeRepository.java
+++ tripant/src/main/java/mclass/store/tripant/plan/model/repostiory/TimeRepository.java
@@ -4,12 +4,11 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import mclass.store.tripant.place.domain.PlaceAreaEntity;
+import mclass.store.tripant.place.domain.PlaceMapEntity;
+
 
 @Mapper
 public interface TimeRepository {
 	public int deleteAllPlaceMoveTime();
-	public List<PlaceAreaEntity> selectAreaCodeList(int areacode);
-	public int selectAreaCodeCount(int areacode);
-	
+	public List<PlaceMapEntity> selectPlaceMapList(int areacode);
 }
