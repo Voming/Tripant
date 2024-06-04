26e38446f731782374aa0dac08abc9ed5b60c542
seojw0730 <seojw0730@naver.com>
2024-06-04 11:22:02 +0900

    재원) 회원가입
    1. 생일 미래 불가
    2. 개발자 도구 감지 시 메인 페이지로 이동
    3. 탈퇴 '처리' --> 탈퇴 '완료'

--- tripant/src/main/resources/static/js/mypage/quit/move.js
+++ tripant/src/main/resources/static/js/mypage/quit/move.js
@@ -12,7 +12,7 @@ function quitClickHandler(){
 		success: function(result){
 			if(result == 1){
 				if(confirm('정말 탈퇴하시겠습니까?')){
-					alert('탈퇴 처리되었습니다.\n메인 페이지로 이동합니다.');
+					alert('탈퇴 완료되었습니다.\n메인 페이지로 이동합니다.');
 					let form = document.getElementById('frm-quit');
 					form.action = '/logout';
 					form.method = 'POST';
