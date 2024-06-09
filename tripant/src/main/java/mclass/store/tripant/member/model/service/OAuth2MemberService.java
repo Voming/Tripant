833fbc01a42195d6b96837667e60781785cc025a
seojw0730 <seojw0730@naver.com>
2024-06-09 14:20:44 +0900

    재원) 로그인
    1. 카카오, 네이버, 구글 로그인만 적용

--- tripant/src/main/java/mclass/store/tripant/member/model/service/OAuth2MemberService.java
+++ tripant/src/main/java/mclass/store/tripant/member/model/service/OAuth2MemberService.java
@@ -13,7 +13,6 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import mclass.store.tripant.member.domain.CustomOAuth2User;
@@ -23,35 +22,38 @@ import mclass.store.tripant.member.model.repository.MemberRepository;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class OAuth2MemberService extends DefaultOAuth2UserService {
 	
 	private final MemberRepository memberRepository;
 	
 	@Override
-		public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-			System.out.println("loadUser===========");
-			OAuth2User oAuth2User = super.loadUser(userRequest);
-			String email;
-			if(userRequest.getClientRegistration().getRegistrationId().equals("kakao")) {
-				Map<String, Object> kakaoAttributes = oAuth2User.getAttribute("kakao_account");
-				email = (String) kakaoAttributes.get("email"); 
-			}else {
-				email = oAuth2User.getAttribute("email");
-			}
-			Optional<MemberEntity> memberEntityOp = Optional.ofNullable(memberRepository.login(email));
-			if(memberEntityOp.isEmpty()) {
-				throw new UsernameNotFoundException("가입해");
-			}
-			MemberEntity memberEntity = memberEntityOp.get();
-			List<GrantedAuthority> authorities = new ArrayList<>();
-			switch(memberEntity.getMemRole()) {
-				case "ROLE_OWNER": authorities.add(new SimpleGrantedAuthority(MemberRole.OWNER.getRole()));
-				case "ROLE_ADMIN": authorities.add(new SimpleGrantedAuthority(MemberRole.ADMIN.getRole()));
-				case "ROLE_VIP": authorities.add(new SimpleGrantedAuthority(MemberRole.VIP.getRole()));
-				case "ROLE_MEM": authorities.add(new SimpleGrantedAuthority(MemberRole.MEM.getRole()));
-			}
-			System.out.println("oAuth2User = "+oAuth2User);
-			return new CustomOAuth2User(email, memberEntity.getMemPassword(), authorities);
+	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+		System.out.println("loadUser===========");
+		OAuth2User oAuth2User = super.loadUser(userRequest);
+		String email = "";
+		if(userRequest.getClientRegistration().getRegistrationId().equals("kakao")) {
+			Map<String, Object> kakaoAttributes = oAuth2User.getAttribute("kakao_account");
+			email = (String) kakaoAttributes.get("email"); 
+		}else if(userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+			Map<String, Object> naverAttributes = oAuth2User.getAttribute("response");
+			email = (String) naverAttributes.get("email"); 
+		}else if(userRequest.getClientRegistration().getRegistrationId().equals("google")){
+			email = (String) oAuth2User.getAttributes().get("email");
 		}
+		System.out.println("email = "+email);
+		Optional<MemberEntity> memberEntityOp = Optional.ofNullable(memberRepository.login(email));
+		if(memberEntityOp.isEmpty()) {
+			throw new UsernameNotFoundException("가입해");
+		}
+		MemberEntity memberEntity = memberEntityOp.get();
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		switch(memberEntity.getMemRole()) {
+			case "ROLE_OWNER": authorities.add(new SimpleGrantedAuthority(MemberRole.OWNER.getRole()));
+			case "ROLE_ADMIN": authorities.add(new SimpleGrantedAuthority(MemberRole.ADMIN.getRole()));
+			case "ROLE_VIP": authorities.add(new SimpleGrantedAuthority(MemberRole.VIP.getRole()));
+			case "ROLE_MEM": authorities.add(new SimpleGrantedAuthority(MemberRole.MEM.getRole()));
+		}
+		System.out.println("oAuth2User = "+oAuth2User);
+		return new CustomOAuth2User(email, memberEntity.getMemPassword(), authorities);
+	}
 }
