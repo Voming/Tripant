9a151da9210c96dd9050cf3f1e22f270a977559d
Voming <ebomin1234@gmail.com>
2024-06-10 18:05:50 +0900

    aspect log 찍기

--- tripant/src/main/java/mclass/store/tripant/config/AspectConfig.java
+++ tripant/src/main/java/mclass/store/tripant/config/AspectConfig.java
@@ -1,4 +1,4 @@
-package mclass.store.tripant.common;
+package mclass.store.tripant.config;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -10,14 +10,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Aspect
 @Component
 public class AspectConfig {
-	private Logger logger = LoggerFactory.getLogger(AspectConfig.class);
-	
 	@Pointcut("execution(public * mclass..*Repository.*(..))")
 	public void RepositoryPointcut() {}
 	
@@ -27,23 +28,42 @@ public class AspectConfig {
 	@Pointcut("execution(public * mclass..*Controller.*(..))")
 	public void controllerPointcut() {}
 
-	@Around("RepositoryPointcut()")
+	@Around("daoPointcut()")
 	public Object aroundDaoLog(ProceedingJoinPoint pjp) throws Throwable {
 		Object returnObj = null;
 		// pjp.getThis() : 클래스명
 		// pjp.getSignature().getName(): 타겟메소드명
-		logger.debug("▷▷▷["+pjp.getThis()+":"+pjp.getSignature().getName()+"]");
+		log.info("▶▶▶["+pjp.getThis()+":"+pjp.getSignature().getName()+"]");
 		// pjp.getArgs() : 메소드의 파라메터 값들이 들어있음.
 		Object[] args = pjp.getArgs();
 		for(int i=0; i<args.length; i++) {
-			logger.debug("▷▷▷-args["+i+"] "+args[i]+"");
+			log.info("▽▽▽-args["+i+"] "+args[i]+"");
 		}
 		StopWatch stopwatch = new StopWatch();
 		stopwatch.start();
 		// pjp.proceed() : 타겟메소드 호출함
 		returnObj = pjp.proceed();
 		stopwatch.stop();
-		logger.debug("▷▷▷[Dao ▷ "+stopwatch.getTotalTimeMillis()+"ms]"+returnObj);
+		log.info("▷▷▷[Dao ▷ "+stopwatch.getTotalTimeMillis()+"ms]"+returnObj);
+		return returnObj;
+	}
+	@Around("servicePointcut()")
+	public Object aroundServiceLog(ProceedingJoinPoint pjp) throws Throwable {
+		Object returnObj = null;
+		// pjp.getThis() : 클래스명
+		// pjp.getSignature().getName(): 타겟메소드명
+		log.info("▶▶["+pjp.getThis()+":"+pjp.getSignature().getName()+"]");
+		// pjp.getArgs() : 메소드의 파라메터 값들이 들어있음.
+		Object[] args = pjp.getArgs();
+		for(int i=0; i<args.length; i++) {
+			log.info("▽▽-args["+i+"] "+args[i]+"");
+		}
+		StopWatch stopwatch = new StopWatch();
+		stopwatch.start();
+		// pjp.proceed() : 타겟메소드 호출함
+		returnObj = pjp.proceed();
+		stopwatch.stop();
+		log.info("▷▷[Svc ▷ "+stopwatch.getTotalTimeMillis()+"ms]"+returnObj);
 		return returnObj;
 	}
 	
@@ -52,31 +72,24 @@ public class AspectConfig {
 		Object returnObj = null;
 		// pjp.getThis() : 클래스명
 		// pjp.getSignature().getName(): 타겟메소드명
-		logger.debug("▷["+pjp.getThis()+":"+pjp.getSignature().getName()+"]");
+		log.info("▶["+pjp.getThis()+":"+pjp.getSignature().getName()+"]");
 		// pjp.getArgs() : 메소드의 파라메터 값들이 들어있음.
 		Object[] args = pjp.getArgs();
 		for(int i=0; i<args.length; i++) {
-			logger.debug("▷-args["+i+"] "+args[i]+"");
+			log.info("▽-args["+i+"] "+args[i]+"");
 		}
 		StopWatch stopwatch = new StopWatch();
 		stopwatch.start();
 		// pjp.proceed() : 타겟메소드 호출함
 		returnObj = pjp.proceed();
 		stopwatch.stop();
-		logger.debug("▷[Ctrl ▷ "+stopwatch.getTotalTimeMillis()+"ms]"+returnObj);
+		
+//		log.info("▷[Ctrl ▷ "+stopwatch.getTotalTimeMillis()+"ms]"+ returnObj);
+
+		//		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		Gson gson = new GsonBuilder().create();
+		log.info("▷[Ctrl ▷ "+stopwatch.getTotalTimeMillis()+"ms]"+ gson.toJson(returnObj));
 		return returnObj;
-	}
-	
-	//@Before("controllerPointcut()")
-	public void bctrlLog(JoinPoint jp) {
-		// pjp.getThis() : 클래스명
-		// pjp.getSignature().getName(): 타겟메소드명
-		logger.debug("▷["+jp.getThis()+":"+jp.getSignature().getName()+"]");
-		// pjp.getArgs() : 메소드의 파라메터 값들이 들어있음.
-		Object[] args = jp.getArgs();
-		for(int i=0; i<args.length; i++) {
-			logger.debug("▷-args["+i+"] "+args[i]+"");
-		}
 	}
 	
 }
