a39736ecd7c0aa073dca5bde4831ad784856b6b8
JJOGGAN <withmelona1@gmail.com>
2024-06-10 19:10:48 +0900

    은진) DML 수정

--- SQL/trip_DML.sql
+++ SQL/trip_DML.sql
@@ -4,11 +4,17 @@ select * from member;
 select * from plan_member;
 desc plan;
 desc plan_member;
-insert into plan values(SEQ_PLAN_ID.nextval,1,'개미와 함께하는 서울여행',sysdate, sysdate+2,sysdate,null);
-insert into plan_member values (22,'gyrua34@gmail.com','1');
+insert into plan values(SEQ_PLAN_ID.nextval,1,'개미와 함께하는 서울여행',sysdate+2, sysdate+5,sysdate,null);
+insert into plan values(SEQ_PLAN_ID.nextval,39,'개미와 함께하는 제주 한라봉봉여행',sysdate+10, sysdate+12,sysdate,null);
+insert into plan values(SEQ_PLAN_ID.nextval,39,'군침루피와 눈뜨고 코베이는 서울여행',sysdate+13, sysdate+20,sysdate,null);
+insert into plan values(SEQ_PLAN_ID.nextval,1,'군침루피와 눈뜨고 코베이는 서울여행2',sysdate+20, sysdate+22,sysdate,null);
+insert into plan_member values (2,'gyrua34@gmail.com','1');
 insert into plan_member values (1,'gyrua34@gmail.com','1');
-insert into plan_member values (2,'seojw0730@naver.com','1');
-insert into plan_member values (23,'seojw0730@naver.com','1');
+insert into plan_member values (4,'gyrua34@gmail.com','1');
+insert into plan_member values (3,'gyrua34@gmail.com','0');
+
+insert into plan_member values (2,'qothwls5@naver.com','0');
+insert into plan_member values (3,'qothwls5@naver.com','1');
 
 --area_code = 1
 --plan_area_code = 1
