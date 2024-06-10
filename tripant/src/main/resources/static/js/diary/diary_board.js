9e042d67ee3ee28c49017322e999299a9a6b3d65
sobae17 <sobae17@naver.com>
2024-06-10 16:59:54 +0900

    지역탭 fragment로 파일 생성, js 파일 적용 오류 해결, 지역태그 클릭시 글씨 색깔 변경 -> 배경색 흰색에서 검은색으로 변경으로 적용

--- tripant/src/main/resources/static/js/diary/diary_board.js
+++ tripant/src/main/resources/static/js/diary/diary_board.js
@@ -1,16 +1,17 @@
 /*탭 전환 시키기  */
+$(document).ready(function(){
 $('.area-tab-nav a').click(function() {
 	$('.diary-tab-content > div').hide().filter(this.hash).fadeIn();
+	$('.area-tab-nav li').css("background-color", "white");
 	$('.area-tab-nav a').removeClass('active');
 	$(this).addClass('active');
+	$(this).parent().css("background-color", "black");
 	return false;
 }).filter(':eq(0)').click();
 
 /*좋아요 누르기  */
 
-$(document)
-	.ready(
-		function() {
+$(document).ready(function() {
 			var itemsPerPage = 8; // 페이지에 8개씩 뿌리기
 			var currentPage = 0;
 			var currentTap = '#tab01'; // 초기 탭은 전체
@@ -30,3 +31,9 @@ $(document)
 						}
 					});
 		});
+		
+/*글 클릭하기*/
+$(document).ready(function(){
+	
+});
+});
