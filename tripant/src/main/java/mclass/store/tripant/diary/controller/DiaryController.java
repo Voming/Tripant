dcfc80c73e50dd9fbe5a463a4b5b397bb5745260
JJOGGAN <withmelona1@gmail.com>
2024-06-11 16:16:01 +0900

    소진) 게시글 신고 모달창

--- tripant/src/main/java/mclass/store/tripant/diary/controller/DiaryController.java
+++ tripant/src/main/java/mclass/store/tripant/diary/controller/DiaryController.java
@@ -9,15 +9,20 @@ import org.springframework.web.servlet.ModelAndView;
 
 import mclass.store.tripant.diary.domain.DiaryBoardEntity;
 import mclass.store.tripant.diary.service.DiaryService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 
 @Controller
+@RequestMapping(value = "/diary")
 public class DiaryController {
 	@Autowired
 	private DiaryService diaryService;
 
 	// 전체 글보기(공개 글)
-	@GetMapping("/diary")
+	@GetMapping("")
 	//modelAndView로 db두번 갔다오지 않게 하기
 	public ModelAndView diary(ModelAndView mv) {
 		mv.addObject("diaries", diaryService.selectDiaryList());
@@ -27,10 +32,16 @@ public class DiaryController {
 	}
 
 	// 글 상세보기
-    @GetMapping("/diary/read/{id}")
+    @GetMapping("/read/{id}")
     public String diartRead(@PathVariable Long id, Model model) {
         DiaryBoardEntity diary = diaryService.findById(id);
         model.addAttribute("diary", diary);
         return "diary/diary_read";
 	}
+//    @PostMapping("/report")
+//    public int report() {
+//        int result = diaryService.
+//        return result;
+//    }
+    
 }
