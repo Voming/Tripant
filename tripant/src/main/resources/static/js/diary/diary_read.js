fd7dfd7678f2a94a2540047c8ca1fce7920cd6b9
sobae17 <sobae17@naver.com>
2024-06-11 14:28:25 +0900

    글 상세보기 페이지에서 햄버거 버튼 만들기

--- tripant/src/main/resources/static/js/diary/diary_read.js
+++ tripant/src/main/resources/static/js/diary/diary_read.js
@@ -1,10 +1,10 @@
 $(loaededHandler);
 function loaededHandler(){
 	//공유-모달창
-	$('.share-plan.modal').on("click",shareModalHandler);
+	//$('.share-plan.modal').on("click",shareModalHandler);
 	
 	//삭제
-	$('.delete-diary').on("click",deleteHandler);
+	//$('.delete-diary').on("click",deleteHandler);
 	
 	//케밥 아이콘 이벤트
 	$('.info').on("click",miniModalBtnHandler);
@@ -17,24 +17,24 @@ function loaededHandler(){
 }
 
 //친구공유 모달
-function shareModalHandler(){
+/*function shareModalHandler(){
 	$(".wrap-modal").css("display","block");
 
-}
+}*/
 //친구공유 모달 닫기
-$(document).mouseup(function(e) {
+/*$(document).mouseup(function(e) {
 	var LayerModal = $(".wrap-modal");
 	if (LayerModal.has(e.target).length === 0) {
 		LayerModal.css("display","none");
 	}
 	$('.mini-modal').removeClass('active');
 	$(this).parent().addClass('active');
-});
+});*/
 
 //미니 모달창
-function miniModalBtnHandler(){
+/*function miniModalBtnHandler(){
 	var mbtn = $(this).children('.mini-modal');
 	$('.mini-modal').addClass('hide');
 	$(this).children('.mini-modal').removeClass("hide");
 	
-}
+}*/
