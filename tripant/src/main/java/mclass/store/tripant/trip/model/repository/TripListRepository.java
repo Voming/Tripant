773efb1afaec76339caa6d462b55dced19c1411f
JJOGGAN <withmelona1@gmail.com>
2024-06-12 12:54:39 +0900

    은진) 유저검색
    유저검색 시 공유의 유무 확인가능
    contorller, service,repository,mapper 연결
    view 확인 필요

--- tripant/src/main/java/mclass/store/tripant/trip/model/repository/TripListRepository.java
+++ tripant/src/main/java/mclass/store/tripant/trip/model/repository/TripListRepository.java
@@ -1,16 +1,22 @@
 package mclass.store.tripant.trip.model.repository;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
 import mclass.store.tripant.trip.domain.TripListEntity;
+import mclass.store.tripant.trip.domain.TripShareEntity;
 
-@Mapper
+@Mapper //mybatis를 사용하기 위한 어노테이션
 public interface TripListRepository {
-	
-	// 여행목록 
+	//나의 일정
+	//목록 
 	public List<TripListEntity>  selectTripList(String memEmail);
+	
+	//삭제
 	public Integer delete(Integer planId);
 	
+	//유저검색
+	public List<TripShareEntity> find(Map<String, Object> map);
 }
