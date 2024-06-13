03311e191b7156c038400c4509cdba61034209e2
Voming <ebomin1234@gmail.com>
2024-06-13 10:28:09 +0900

    지역 선택하세요 비활성화 및 일정 만들고있을때 새로고침 방지

--- tripant/src/main/resources/static/js/plan/plan_make.js
+++ tripant/src/main/resources/static/js/plan/plan_make.js
@@ -3,26 +3,44 @@ $(loadedHandler);
 function loadedHandler() {
 	$(".logo").on("click", btnLogoClickHandler);
 	$(".plan-priod").on("click", btnPriodClickHandler);
-	
+
 	//좌측 탭
 	$('.tab-nav a').click(function() {
 		$('.tab-content > div').hide().filter(this.hash).fadeIn();
-		$('.tab-nav a').css("color","black");
+		$('.tab-nav a').css("color", "black");
 		$('.tab-nav a').removeClass('active');
 		$(this).addClass('active');
-	
+
 		$(this).css("color", "#4BC9E5");
 		return false;
 	}).filter(':eq(0)').click();
 }
 
-function btnLogoClickHandler(){
-	if( confirm("지금 나가면 작업한 내용이 없어집니다. 괜찮습니까?") ==true){
+document.onkeydown = fkey;
+var wasPressed = false;
+
+function fkey(e) {
+	e = e || window.event;
+	if (wasPressed) return;
+	if (e.keyCode == 116) { 
+		if (confirm("지금 나가면 작업한 내용이 없어집니다. 괜찮습니까?") == true) {
+			location.reload(true);
+		} else{
+			 event.preventDefault();
+		}
+		
+	}
+}
+
+function btnLogoClickHandler() {
+	if (confirm("지금 나가면 작업한 내용이 없어집니다. 괜찮습니까?") == true) {
 		location.href = "/";
 	}
 }
 
-function btnPriodClickHandler(){
+function btnPriodClickHandler() {
 	history.go(0);
 	$(".modal").addClass("show");
 }
+
+
