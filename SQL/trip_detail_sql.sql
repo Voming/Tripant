4202453475d889bbea43ec9d12b80dfe50434a98
JJOGGAN <withmelona1@gmail.com>
2024-06-16 23:13:38 +0900

    은진) sql 여행 세부 일정 정보
    planId에 따른 여행 장소 세부 정보 불러오기

---
+++ SQL/trip_detail_sql.sql
@@ -0,0 +1,5 @@
+select stay_day travel_date,stay_plan_id plan_id,stay_type type,null travel_order,null stay_time,null spot_memo,p.* from plan_stay join (select contentid,title , add1 address,firstimage,mapx lng,mapy lat from place) p on stay_contentid= p.contentid where stay_plan_id  = 13
+union
+select s.spot_day travel_date, s.spot_plan_id,s.spot_type,s.spot_order ,s.spot_stay_time,spot_memo,p.* from plan_spot s join (select contentid,title , add1 address,firstimage,mapx lng,mapy lat from place) p on s.spot_contentid = p.contentid where s.spot_plan_id = 13
+order by travel_date asc ,travel_order asc;
+desc plan_spot;
