85c38e0071b69942b816b471a53bc645cb83d2aa
JJOGGAN <withmelona1@gmail.com>
2024-06-17 18:52:57 +0900

    은진) controller 불필요한 import값 제거

--- tripant/src/main/java/mclass/store/tripant/trip/controller/TripListController.java
+++ tripant/src/main/java/mclass/store/tripant/trip/controller/TripListController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import mclass.store.tripant.trip.domain.TripShareEntity;
 import mclass.store.tripant.trip.model.service.TripListService;
