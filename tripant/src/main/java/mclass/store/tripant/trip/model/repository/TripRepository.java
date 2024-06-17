9b6315390ecbe1614be2e6c2eb6a8380b3ec3f38
JJOGGAN <withmelona1@gmail.com>
2024-06-17 14:13:54 +0900

    은진) 여행 세부일정 DB부터 controller까지 연결
    sql문 작성하여 controller까지 ModelAndView로 연결 html에 연결 필요

--- tripant/src/main/java/mclass/store/tripant/trip/model/repository/TripRepository.java
+++ tripant/src/main/java/mclass/store/tripant/trip/model/repository/TripRepository.java
@@ -1,8 +1,16 @@
 package mclass.store.tripant.trip.model.repository;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
-@Mapper
-public class TripRepository {
+import mclass.store.tripant.trip.domain.DayEntity;
 
+@Mapper
+public interface TripRepository {
+	
+	//여행세부일정 목록
+	public List<DayEntity> detailInfo(Integer planId);
+	
+	
 }
--- tripant/src/main/java/mclass/store/tripant/trip/model/service/TripService.java
+++ tripant/src/main/java/mclass/store/tripant/trip/model/service/TripService.java
@@ -1,8 +1,20 @@
 package mclass.store.tripant.trip.model.service;
 
+import java.util.List;
 
-/*@Service
-@RequiredArgsConstructor*/
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import mclass.store.tripant.trip.domain.DayEntity;
+import mclass.store.tripant.trip.model.repository.TripRepository;
+
+@Service
+@RequiredArgsConstructor
 public class TripService {
-	//private final TripRepository repository;
+	private final TripRepository repository;
+	
+	//여행세부일정 목록
+	public List<DayEntity> detailInfo(Integer planId){
+		return repository.detailInfo(planId);
+	};
 }
