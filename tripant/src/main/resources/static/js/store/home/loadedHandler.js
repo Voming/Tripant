266615e77c69979f52690201c17c7437ff3e875c
seojw0730 <seojw0730@naver.com>
2024-06-20 15:58:39 +0900

    재원) 스토어
    1. 글꼴 미리보기 창 sweetalert2 로 변경

--- tripant/src/main/resources/static/js/store/home/loadedHandler.js
+++ tripant/src/main/resources/static/js/store/home/loadedHandler.js
@@ -1,5 +1,6 @@
 $(loadedHandler);
 function loadedHandler(){
+	AOS.init();
 	// 버튼 호버 css
 	$('.btn').hover(function(){
 		$(this).css('background-color', 'var(--color_light_gray)');
@@ -12,11 +13,12 @@ function loadedHandler(){
 	// 상품 선택 시 개수 표현
 	$('input').on('click', itemNumHandler);
 	
-	// 폰트 모달 띄우기
-	$('.btn.font').on('click', modalOpenHandler);
+	// 글꼴 미리보기 창
+	// $('.btn.font').on('click', modalOpenHandler);
+	$('.btn.font').on('click', fontSwalHandler);
 	
-	// 폰트 모달 닫기
-	$('.btn.modal-close').on('click', modalCloseHandler);
+	// 글꼴 모달 닫기
+	// $('.btn.modal-close').on('click', modalCloseHandler);
 	
 	// 장바구니에 담기
 	$('.btn.cart').on('click', insertItemsHandler);
