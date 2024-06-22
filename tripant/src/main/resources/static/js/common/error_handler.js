40a5a70166263bae052d61d48a70a0ec80473baf
seojw0730 <seojw0730@naver.com>
2024-06-22 11:25:00 +0900

    재원)~단순 수정
    1. ajaxErrorHandler icon 추가

--- tripant/src/main/resources/static/js/common/error_handler.js
+++ tripant/src/main/resources/static/js/common/error_handler.js
@@ -1,7 +1,8 @@
 //ajax error 부분
 function ajaxErrorHandler (){
 	Swal.fire({
-		title: "알 수 없는 오류가 발생했습니다.\n관리자에게 문의해주시기 바랍니다.", 
+		text: "알 수 없는 오류가 발생했습니다. 관리자에게 문의해주시기 바랍니다.",
+		icon: "error", 
 		confirmButtonColor: "#000000", 
 		confirmButtonText: "확인"
 	});
