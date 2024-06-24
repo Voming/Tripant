bcda6dd5af67641b534a048dfbd063ac48cbdb71
Voming <ebomin1234@gmail.com>
2024-06-24 09:38:18 +0900

    선택한 장소 마커 저장

--- tripant/src/main/resources/static/js/plan/plan_spot_check.js
+++ tripant/src/main/resources/static/js/plan/plan_spot_check.js
@@ -1,50 +1,39 @@
 //체크박스 클릭
 function spotCkBtnClickHandler(thisElement) {
-	console.log(thisElement);
 	var latx = $(thisElement).parent().find(".spot-x").attr("value");
 	var lngy = $(thisElement).parent().find(".spot-y").attr("value");
 
-	console.log(latx + ":" + lngy);
-
 	var moveLatLon = new kakao.maps.LatLng(lngy, latx);
 	// 확대 크기 변경
-	map.setLevel(6);
-	// 지도 이동
+	//map.setLevel(6);
+	// 지도 이동 setCenter()바로 이동) -  panTo()(부드럽게 이동)
 	map.setCenter(moveLatLon);
-	// 지도에 마커를 표시합니다
-	marker.setMap(moveLatLon);
-	// 마커 위치를 클릭한 위치로 옮깁니다
-	marker.setPosition(moveLatLon);
 
-
+	//마커가 담길 배열
+	var positions = [];
 	//체크된 리스트 반복 
 	$(".wrap-spotList").find('input:checked').each(function(index) {
 		var title = $(this).parent().find(".spot-name").attr("value");
 		var mapx = $(this).parent().find(".spot-x").attr("value");
 		var mapy = $(this).parent().find(".spot-y").attr("value");
-
-		calendarPlan.dateArr[index] = new spot(title, mapx, mapy);
+		calendarPlan.spotArr[index] = new spot(title, mapx, mapy);  //전체 일정 만들기 정보 저장
+		positions.push({ latlng: new kakao.maps.LatLng(mapy, mapx) }); //마커 표시할 위치 저장
 	});
+	//console.log(calendarPlan);
 
-	console.log(calendarPlan.dateArr);
+	// 마커 이미지의 이미지 주소입니다
+	var imageSrc = "/images/loacation/location5.png";
+	for (var i = 0; i < positions.length; i++) {
+		// 마커 이미지의 이미지 크기 입니다
+		var imageSize = new kakao.maps.Size(24, 26);
+		// 마커 이미지를 생성합니다    
+		var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+		// 마커를 생성합니다
+		var marker = new kakao.maps.Marker({
+			map: map, // 마커를 표시할 지도
+			position: positions[i].latlng, // 마커를 표시할 위치
+			title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+			image: markerImage // 마커 이미지 
+		});
+	}
 }
-
-
-
-// 바로 이동
-function setCenter() {
-	// 이동할 위도 경도 위치를 생성합니다 
-	var moveLatLon = new kakao.maps.LatLng(37.5670686420, 127.0101708978);
-
-	// 지도 중심을 이동 시킵니다
-	map.setCenter(moveLatLon);
-}
-// 부드럽게 이동
-function panTo() {
-	// 이동할 위도 경도 위치를 생성합니다 
-	var moveLatLon = new kakao.maps.LatLng(33.450580, 126.574942);
-
-	// 지도 중심을 부드럽게 이동시킵니다
-	// 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
-	map.panTo(moveLatLon);
-}  
