b4872f0806560d6defb7478163b2631bc9e3ec7a
sobae17 <sobae17@naver.com>
2024-06-26 17:57:06 +0900

    글작성 시 바로 이미지파일 서버 연결 성공

--- tripant/src/main/java/mclass/store/tripant/diary/domain/DiaryBoardEntity.java
+++ tripant/src/main/java/mclass/store/tripant/diary/domain/DiaryBoardEntity.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
+import mclass.store.tripant.diary.service.StringLengthApi;
 
 
 @Data
@@ -26,21 +27,21 @@ public class DiaryBoardEntity {
 	private Integer diaryPlanId;
 	private String diaryTitle;
 	private String diaryContent;
-	private List<String> diaryContentArrList;
+//	private List<String> diaryContentArrList;
 	private String diaryDate;
 	private String diaryOpen;
 	private Integer diaryViews;
 	private String diaryTheme;
 	private String memNick;
 
-	public void setDiaryContent(String diaryContent) {
-		this.diaryContent = diaryContent;
-
-		// 추가로 UTF-8 - 4000byte List<String> diaryContentArrList
-		//this.diaryContentArrList = StringLengthApi.getMaxByteStringList(diaryContent, 4000, 1000);
+//	public void setDiaryContent(String diaryContent) {
+//		this.diaryContent = diaryContent;
+//
+//		// 추가로 UTF-8 - 4000byte List<String> diaryContentArrList
+//		this.diaryContentArrList = StringLengthApi.getMaxByteStringList(diaryContent, 4000, 1000);
 //		for (String item : diaryContentArrList) {
 //			System.out.println("---- : "+ item.getBytes().length);
 //			System.out.println(item);
 //		}
-	}
+//	}
 }
