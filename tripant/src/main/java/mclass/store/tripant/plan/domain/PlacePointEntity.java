65e3c7734554c8ee250509e18e82c086775b347c
Voming <ebomin1234@gmail.com>
2024-07-02 18:11:38 +0900

    일정 생성 위한 Entity 생성

---
+++ tripant/src/main/java/mclass/store/tripant/plan/domain/PlacePointEntity.java
@@ -0,0 +1,15 @@
+package mclass.store.tripant.plan.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlacePointEntity {
+	private Integer index;
+	private String contentid;
+    private String mapx;         
+    private String mapy;   
+}
