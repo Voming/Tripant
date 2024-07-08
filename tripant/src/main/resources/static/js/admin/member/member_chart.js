b010396d259481c17942f321449072988df6308a
oyeee <dpdls898@naver.com>
2024-07-08 12:49:35 +0900

    관리자 페이지 회원통계 ~ing
    chart.js 넣기

--- tripant/src/main/resources/static/js/admin/member/member_chart.js
+++ tripant/src/main/resources/static/js/admin/member/member_chart.js
@@ -7,21 +7,33 @@ var ctx2 = document.getElementById('myChart2').getContext('2d');
 var myChart2 = new Chart(ctx2, {
 		  type: 'bubble',
 		  data: {        
-				labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],        
+				labels: ["1월", "2월", "3월", "4월", "5월", "6월"],        
 				datasets: [{           
 					label: '회원수',       
-					 data: [{x:20,y:30,r:15},{x:30,y:10,r:10}],            
+					 data: [{x:3,y:5,r:7},{x:5,y:7,r:7},{x:4,y:4,r:8}
+					 ,{x:6,y:3,r:5},{x:8,y:3,r:6},{x:9,y:9,r:9}],            
 					backgroundColor: [                
-						'rgba(255, 99, 132, 0.2)',                
+						'rgba(255, 99, 132, 0.2)',               
 						'rgba(54, 162, 235, 0.2)',                
 						'rgba(255, 206, 86, 0.2)',                
 						'rgba(75, 192, 192, 0.2)',                
 						'rgba(153, 102, 255, 0.2)',               
-						'rgba(255, 159, 64, 0.2)'            
+						'rgba(255, 159, 64, 0.2)'           
 						],            
-							}]    
-						}
-		 ,
+							},{
+					label: '일정생성수',  
+					 data: [{x:1,y:5,r:7},{x:7,y:7,r:7},{x:9,y:3,r:8}
+					 ,{x:7,y:9,r:5},{x:3,y:3,r:6},{x:5,y:9,r:9}],            
+					backgroundColor: [                
+						'rgba(255, 99, 132, 0.8)',               
+						'rgba(54, 162, 235, 0.8)',                
+						'rgba(255, 206, 86, 0.8)',                
+						'rgba(75, 192, 192, 0.8)',                
+						'rgba(153, 102, 255, 0.8)',               
+						'rgba(255, 159, 64, 0.8)'           
+						],
+							}]  
+						},
 		  options: {
 		    responsive: true,
 		    plugins: {
