05412a429a57d7a4a984a968cf5a8cd6789c8cd9
oyeee <dpdls898@naver.com>
2024-07-10 13:43:52 +0900

    관리자 페이지 css 추가
    페이지 버튼 css 추가

--- tripant/src/main/resources/static/js/admin/board/board_chart.js
+++ tripant/src/main/resources/static/js/admin/board/board_chart.js
@@ -32,25 +32,25 @@ var myChart = new Chart(ctx, {
 							}]    
 						}
 		 ,
-		 /* backgroundColor: [                
-				'rgba(255, 99, 132, 0.2)',                
+		  		backgroundColor: [                
+						'rgba(255, 99, 132, 0.2)',                
 						'rgba(54, 162, 235, 0.2)',                
 						'rgba(255, 206, 86, 0.2)',                
 						'rgba(75, 192, 192, 0.2)',                
 						'rgba(153, 102, 255, 0.2)',               
 						'rgba(255, 159, 64, 0.3)',            
-						'#E54B4B80', 
+						'#CC333359', 
 						'#C943CC33' ,          
-						'#F0FCD180' ,     
-						'#FFA7C5' ,
-						'#E5794B99' ,
-						'#C9EFF7' ,
+						'#CCCCCC73' ,     
+						'#CCCCFF' ,
+						'#CCCCFF59' ,
+						'#4B76E559' ,
 						'#4B76E599' ,
-						'#2EB67D80',
+						'#6699CCB3',
 						'#E5C34B80',
-						'#E5C34B',
-						'#13358F99'         
-				],   */  
+						'#FF990073',
+						'#0066FF59'         
+				],    
 		  options: {
 		    responsive: false,
 		    plugins: {
