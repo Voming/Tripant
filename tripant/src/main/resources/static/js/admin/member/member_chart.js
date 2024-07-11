36f8a3974d9a79514461c580767025031b7ed3c0
oyeee <dpdls898@naver.com>
2024-07-11 13:00:27 +0900

    관리자 페이지 수정
    회원통계 데이터 값 +1

--- tripant/src/main/resources/static/js/admin/member/member_chart.js
+++ tripant/src/main/resources/static/js/admin/member/member_chart.js
@@ -10,17 +10,17 @@ var ctx2 = document.getElementById('myChart2').getContext('2d');
 var myChart2 = new Chart(ctx2, {
 	type: 'line',
  	data: {
-  		labels:["5달전", "4달전", "3달전", "2달전", "1달전", "이번달"],
+  		labels:["6개월전","5달전", "4달전", "3달전", "2달전", "1달전", "이번달"],
 	  	datasets: [{
-			 label: '신규가입회원수',
-			 data: [count5, count4, count3, count2, count1, count0],
+			 label: '신규가입',
+			 data: [count6,count5, count4, count3, count2, count1, count0],
 			 fill: false,
 			 borderColor: 'rgb(255, 99, 132)',
 			 borderWidth:1,
 			 tension: 0.1
 	  },{
 			 label: '일정생성수',
-			 data: [plan5, plan4, plan3, plan2, plan1, plan0],
+			 data: [plan6,plan5, plan4, plan3, plan2, plan1, plan0],
 			 fill: false,
 			 borderColor: '#4B76E5',
 			 borderWidth:1,
