f4d8f1e805a04f163a54ea296271d240af626e50
JJOGGAN <withmelona1@gmail.com>
2024-07-11 17:05:39 +0900

    은진) 여행일정 DML insert 값 변경
    plan_id는 시퀀스 값으로 들어가기 때문에 시퀀스 값이 순차적으로 들어갈 수 있게 큰 number값을 plan_id로 지정

--- SQL/plan_dml.sql
+++ SQL/plan_dml.sql
@@ -11,17 +11,11 @@ desc plan;
 
 select * from plan;
 
-insert INTO plan VALUES (SEQ_PLAN_ID.nextval, 1, '멋진 여행2', DEFAULT, sysdate + 4, DEFAULT, null);
-
-update plan set plan_end_day = (select plan_start_day from plan where plan_id=1) +3  where plan_id=1;
-
-
-alter TABLE plan MODIFY plan_end_day not null;
-
-
-insert INTO plan_schedule VALUES ( (select plan_start_day from plan where plan_id=1), 1, TO_DATE('2024-05-30 10:00:00','YYYY-MM-DD HH24:MI:SS') ,  TO_DATE('2024-05-30 22:00:00','YYYY-MM-DD HH24:MI:SS') );
-insert INTO plan_schedule VALUES ( (select plan_start_day from plan where plan_id=1)+1, 1, TO_DATE('2024-05-30 10:00:00','YYYY-MM-DD HH24:MI:SS') ,  TO_DATE('2024-05-30 22:00:00','YYYY-MM-DD HH24:MI:SS') );
-insert INTO plan_schedule VALUES ( (select plan_start_day from plan where plan_id=1)+2, 1, TO_DATE('2024-05-30 10:00:00','YYYY-MM-DD HH24:MI:SS') ,  TO_DATE('2024-05-30 22:00:00','YYYY-MM-DD HH24:MI:SS') );
+--plan, schedule dml 넣기
+--insert INTO plan VALUES (9999, 1, '멋진 여행2', DEFAULT, sysdate + 2, DEFAULT, null);
+--insert INTO plan_schedule VALUES ( (select plan_start_day from plan where plan_id=9999), 9999, TO_DATE('2024-05-30 10:00:00','YYYY-MM-DD HH24:MI:SS') ,  TO_DATE('2024-05-30 22:00:00','YYYY-MM-DD HH24:MI:SS') );
+--insert INTO plan_schedule VALUES ( (select plan_start_day from plan where plan_id=9999)+1, 9999, TO_DATE('2024-05-30 10:00:00','YYYY-MM-DD HH24:MI:SS') ,  TO_DATE('2024-05-30 22:00:00','YYYY-MM-DD HH24:MI:SS') );
+--insert INTO plan_schedule VALUES ( (select plan_start_day from plan where plan_id=9999)+2, 9999, TO_DATE('2024-05-30 10:00:00','YYYY-MM-DD HH24:MI:SS') ,  TO_DATE('2024-05-30 22:00:00','YYYY-MM-DD HH24:MI:SS') );
 
 update plan_schedule set schedule_start = (TO_DATE(TO_CHAR(schedule_day,'YYYY-MM-DD') || ' 11:00:00','YYYY-MM-DD HH24:MI:SS')) , schedule_end =  (TO_DATE(TO_CHAR(schedule_day,'YYYY-MM-DD') || ' 22:00:00','YYYY-MM-DD HH24:MI:SS')) where schedule_day = TO_DATE('2024-05-31 09:24:37','YYYY-MM-DD HH24:MI:SS');
 update plan_schedule set schedule_start = (TO_DATE('2024-06-01 10:00:00','YYYY-MM-DD HH24:MI:SS')) , schedule_end = (TO_DATE('2024-06-01 22:00:00','YYYY-MM-DD HH24:MI:SS')) where schedule_day = TO_DATE('2024-06-01 09:24:37','YYYY-MM-DD HH24:MI:SS');
@@ -29,6 +23,8 @@ update plan_schedule set schedule_start = (TO_DATE('2024-06-01 10:00:00','YYYY-M
 
 select * from plan;
 select * from plan_schedule;
+--update plan set plan_end_day = (select plan_start_day from plan where plan_id=1) +3  where plan_id=1;
+--alter TABLE plan MODIFY plan_end_day not null;
 
 CREATE OR REPLACE NONEDITIONABLE PROCEDURE pro_INSERT_SCHEDULE (
     schedule_plan_id plan.plan_id%TYPE
