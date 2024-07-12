f6236887413c32b48821a08838fc97b8ebaab0af
seojw0730 <seojw0730@naver.com>
2024-07-12 12:48:00 +0900

    재원) exception
    1. member, store 관련 service, repository에 throws 추가

--- tripant/src/main/java/mclass/store/tripant/member/model/service/MemberService.java
+++ tripant/src/main/java/mclass/store/tripant/member/model/service/MemberService.java
@@ -2,6 +2,7 @@ package mclass.store.tripant.member.model.service;
 
 import java.util.Map;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,74 +18,74 @@ public class MemberService {
 	
 	// 로그인/로그아웃
 	// 로그인
-	public MemberEntity login(String memEmail) {
+	public MemberEntity login(String memEmail) throws DataAccessException {
 		return memberRepository.login(memEmail);
 	}
 	
 	// 토큰 값 가져오기
-	public Map<String, Object> tokenValue(String memEmail){
+	public Map<String, Object> tokenValue(String memEmail) throws DataAccessException {
 		return memberRepository.tokenValue(memEmail);
 	}
 	
 	// SNS 유형 갱신
-	public int updateType(Map<String, Object> map) {
+	public int updateType(Map<String, Object> map) throws DataAccessException {
 		return memberRepository.updateType(map);
 	}
 	
 	// 로그인 기록
-	public int log(Map<String, Object> map) {
+	public int log(Map<String, Object> map) throws DataAccessException {
 		return memberRepository.log(map);
 	}
 	
 	// 휴면 해제
-	public int awake(String memEmail) {
+	public int awake(String memEmail) throws DataAccessException {
 		return memberRepository.awake(memEmail);
 	}
 
 	// 회원가입
 	// 회원가입
-	public Integer join(MemberEntity memberEntity) {
+	public Integer join(MemberEntity memberEntity) throws DataAccessException {
 		return memberRepository.join(memberEntity);
 	}
 
 	// 닉네임 중복 여부
-	public Integer existNick(String memNick) {
+	public Integer existNick(String memNick) throws DataAccessException {
 		return memberRepository.existNick(memNick);
 	}
 
 	// 이메일 가입 여부
-	public Integer existEmail(String memEmail) {
+	public Integer existEmail(String memEmail) throws DataAccessException {
 		return memberRepository.existEmail(memEmail);
 	}
 
 	// 비밀번호 재설정
-	public int setPwd(Map<String, Object> map) {
+	public int setPwd(Map<String, Object> map) throws DataAccessException {
 		return memberRepository.setPwd(map);
 	}
 
 	// 마이페이지
 	// 내 정보
-	public Map<String, Object> myInfo(String memEmail){
+	public Map<String, Object> myInfo(String memEmail) throws DataAccessException {
 		return memberRepository.myInfo(memEmail);
 	}
 	// 닉네임 변경
-	public int saveNick(Map<String, Object> map) {
+	public int saveNick(Map<String, Object> map) throws DataAccessException {
 		return memberRepository.saveNick(map);
 	}
 	
 	// 비밀번호 변경
-	public int savePwd(Map<String, Object> map){
+	public int savePwd(Map<String, Object> map)throws DataAccessException {
 		return memberRepository.savePwd(map);
 	}
 	
 	// 현재 비밀번호
-	public String currPwd(String memEmail) {
+	public String currPwd(String memEmail) throws DataAccessException {
 		return memberRepository.currPwd(memEmail);
 	};
 	
 	// 회원 탈퇴
 	@Transactional
-	public int memQuit(String memEmail) {
+	public int memQuit(String memEmail) throws DataAccessException {
 		int result = memberRepository.memQuit(memEmail);
 		return result;
 	}
--- tripant/src/main/java/mclass/store/tripant/store/model/service/StoreService.java
+++ tripant/src/main/java/mclass/store/tripant/store/model/service/StoreService.java
@@ -3,6 +3,7 @@ package mclass.store.tripant.store.model.service;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,7 +19,7 @@ public class StoreService {
 
 	// 스토어
 	// 테마 목록
-	public List<ItemEntity> themeList(String memEmail) {
+	public List<ItemEntity> themeList(String memEmail) throws DataAccessException {
 		return storeRepository.themeList(memEmail);
 	}
 
@@ -29,7 +30,7 @@ public class StoreService {
 	
 	// 장바구니에 담기
 	@Transactional
-	public int insertItems(String memEmail, Map<String, Object> map) {
+	public int insertItems(String memEmail, Map<String, Object> map) throws DataAccessException {
 		List<String> list = (List<String>) map.get("list");
 		boolean isFont = false;
 		for(int i = 0; i < list.size(); i++) {
@@ -46,29 +47,29 @@ public class StoreService {
 	
 	// 장바구니
 	// 장바구니 목록
-	public List<Map<String, Object>> cart(String memEmail){
+	public List<Map<String, Object>> cart(String memEmail)throws DataAccessException {
 		return storeRepository.cart(memEmail);
 	}
 	
 	// 장바구니 삭제
-	public int cartDel(Map<String, Object> map) {
+	public int cartDel(Map<String, Object> map) throws DataAccessException {
 		return storeRepository.cartDel(map);
 	}
 	
 	// 구매내역
 	// 구매내역 목록
-	public List<Map<String, Object>> buy(String memEmail){
+	public List<Map<String, Object>> buy(String memEmail)throws DataAccessException {
 		return storeRepository.buy(memEmail);
 	}
 	
 	// 구매내역 주문번호
-	public Map<String, Object> buyInfo(String memEmail) {
+	public Map<String, Object> buyInfo(String memEmail) throws DataAccessException {
 		return storeRepository.buyInfo(memEmail);
 	}
 	
 	// 구매내역 추가
 	@Transactional
-	public int pay(Map<String, Object> map) {
+	public int pay(Map<String, Object> map) throws DataAccessException {
 		storeRepository.beforePay(map);
 		int result = storeRepository.pay(map);
 		return result;
